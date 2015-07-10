0110c38fd60bd1157acc2b4e93d79c512506ed71
Kevin Brandstatter <kevin@kbrandstatter.com>
2015-07-09 23:51:12 -0500

    little more robust, with prompts for host and a basic desktop file

--- remote-open.sh
+++ remote-open.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 REMOTE_ADDR=$1
+if [ -z "$REMOTE_ADDR" ]; then
+   REMOTE_ADDR=`zenity --entry --text="Enter the host string"`
+fi
 # Reverse tunnel
-ssh -N $1 -R 0.0.0.0:2222:localhost:22
+ssh -N $REMOTE_ADDR -R 0.0.0.0:2222:localhost:22 &
+PID=$!
+sleep 2
+if kill -0 $PID; then
+   zenity --info --text="Click ok to close the connection"
+   kill $PID
+else
+   zenity --info --text="Failed to establish connection"
+fi
